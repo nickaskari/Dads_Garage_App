447616fe49c626c47e886111727bf9c583f2fa8b
Nick <nickask12@gmail.com>
2022-06-11 20:02:36 +0200

    Initial Commit

---
+++ Dads Garage App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dads Garage App
+//
+//  Created by Nick Askari on 11/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dads Garage App/Dads_Garage_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dads_Garage_AppApp.swift
+//  Dads Garage App
+//
+//  Created by Nick Askari on 11/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Dads_Garage_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
